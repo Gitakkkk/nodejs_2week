d1ef9da4c8f8fe93b400bd41ff22900e8795ff21
nagitak <97418591+nagitak@users.noreply.github.com>
2022-02-12 16:30:59 +0900

    Update comment.js

--- schemas/comment.js
+++ schemas/comment.js
@@ -1,23 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const CommentSchema = new mongoose.Schema({
   nickname: {
     type: String,
   },
-  
+
   posting_url: {
     type: String,
   },
 
   comment: {
-      type: String,
+    type: String,
   },
 });
 
-// UserSchema.virtual("userId").get(function () {
-//   return this._id.toHexString();
-// });
-// UserSchema.set("toJSON", {
-//   virtuals: true,
-// });
-module.exports = mongoose.model("Commenting", CommentSchema);
+module.exports = mongoose.model('Commenting', CommentSchema);
