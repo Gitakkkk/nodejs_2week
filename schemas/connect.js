741cd71aab712dae6308758c5b30439c5d7b6ab6
nagitak <97418591+nagitak@users.noreply.github.com>
2022-02-12 16:31:14 +0900

    Update connect.js

--- schemas/connect.js
+++ schemas/connect.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connect = () => {
-    mongoose.connect("mongodb://localhost:27017/myblog").catch((err) => {
-        console.error(err);
-    });
+  mongoose.connect('mongodb://localhost:27017/myblog').catch((err) => {
+    console.error(err);
+  });
 };
 
 module.exports = connect;
